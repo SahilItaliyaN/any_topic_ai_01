a2d1a0cd02f649a32b7d5c3e2d7c6ce46b7390af
SahilItaliyaN <152978347+SahilItaliyaN@users.noreply.github.com>
2025-06-30 19:49:51 +0530

    complete the companion page
    companion page with filter of topic and subject

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,8 +1,34 @@
+import CompanionCard from '@/components/CompanionCard';
+import SearchInput from '@/components/SearchInput';
+import SubjectFilter from '@/components/SubjectFilter';
+import { getAllCompanion } from '@/lib/actions/companion.action';
+import { getSubjectColor } from '@/lib/utils';
 import React from 'react'
 
-const CompanionLibrary = () => {
+const CompanionLibrary = async({searchParams}:SearchParams) => {
+
+  const filter = await searchParams;
+  const subject = filter.subject?filter.subject:"";
+  const topic = filter.topic?filter.topic:"";
+
+  const companions = await getAllCompanion({subject,topic})
+  console.log(companions)
+
   return (
-    <div>CompanionLibrar</div>
+    <main>
+      <section className='flex justify-between gap-4 max-sm:flex-col' >
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">
+          <SearchInput />
+          <SubjectFilter />
+        </div>
+      </section>
+      <section className="companion-grid">
+        {companions.map((companion)=>(
+          <CompanionCard key={companion.id} {...companion} color={getSubjectColor(companion.subject)} />
+        ))}
+      </section>
+    </main>
   )
 }
 
---
+++ components/SubjectFilter.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { formUrlQuery, removeKeysFromUrlQuery } from '@jsmastery/utils';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
+import { subjects } from '@/constants';
+
+const SubjectFilter = () => {
+
+  const pathname = usePathname();
+    const router = useRouter()
+    const searchParams = useSearchParams();
+    const query = searchParams.get("subject") || "";
+  
+    const [subject , setSubject] = useState(query);
+    
+    useEffect(()=>{
+      let newurl = "";
+      if(subject == "all"){
+        const newUrl = removeKeysFromUrlQuery({
+          params: searchParams.toString(),
+          keysToRemove: ["subject"],
+        });
+      }else{
+        newurl = formUrlQuery({
+          params:searchParams.toString(),
+          key:"subject",
+          value:subject
+        })
+      }
+      router.push(newurl,{scroll:false});
+
+    },[subject,router,searchParams,pathname])
+
+  return (
+    <Select onValueChange={setSubject} value={subject}>
+      <SelectTrigger className='input capitalize'>
+        <SelectValue placeholder="Subject" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="all">
+          All Subject
+        </SelectItem>
+        {subjects.map((subject)=>(
+          <SelectItem key={subject} value={subject} className='capitalize'>
+            {subject}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  )
+}
+
+export default SubjectFilter
