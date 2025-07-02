1e28534b761a0a34fae3632e8a82555e8c0e894c
SahilItaliyaN <152978347+SahilItaliyaN@users.noreply.github.com>
2025-07-02 11:52:21 +0530

    Sentry setup done

--- app/page.tsx
+++ app/page.tsx
@@ -7,43 +7,45 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Popular Companions</h1>
-      <section className='home-section'>
-        <CompanionCard 
-          id="123"
-          name="Neura the Brainy Explorer"
-          topic="Neura Network of the Brain"
-          subject="science"
-          duration={45}
-          color="#ffda6e"
-        />
-        <CompanionCard 
-          id="456"
-          name="Countsy the Number Wizard"
-          topic="Derivatives & Integrals"
-          subject="maths"
-          duration={30}
-          color="#e5d0ff"
-        />
-        <CompanionCard 
-          id="789"  
-          name="Verba the vocabulary Builder"
-          topic="language"
-          subject="english literature"
-          duration={30}
-          color="#BDE7FF"
-        />
-      </section>
-      <section className='home-section'>
-        <CompanionList 
-          title="Recently completed session"
-          companions={recentSessions}
-          className="w-2/3 max-lg:w-full"
-        />
-        <CTA />
-      </section>
-    </div>
+    <main>
+      <div>
+        <h1 className='text-2xl underline'>Popular Companions</h1>
+        <section className='home-section'>
+          <CompanionCard 
+            id="123"
+            name="Neura the Brainy Explorer"
+            topic="Neura Network of the Brain"
+            subject="science"
+            duration={45}
+            color="#ffda6e"
+          />
+          <CompanionCard 
+            id="456"
+            name="Countsy the Number Wizard"
+            topic="Derivatives & Integrals"
+            subject="maths"
+            duration={30}
+            color="#e5d0ff"
+          />
+          <CompanionCard 
+            id="789"  
+            name="Verba the vocabulary Builder"
+            topic="language"
+            subject="english literature"
+            duration={30}
+            color="#BDE7FF"
+          />
+        </section>
+        <section className='home-section'>
+          <CompanionList 
+            title="Recently completed session"
+            companions={recentSessions}
+            className="w-2/3 max-lg:w-full"
+          />
+          <CTA />
+        </section>
+      </div>
+  </main>
   )
 }
 
